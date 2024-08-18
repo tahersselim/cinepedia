608bc3af132abe5a76482c39a0b17ce0ce26dc82
tahersselim <taher.selim.dev@gmail.com>
2024-08-18 15:03:04 +0300

    init

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import Card from "./components/card/Card";
-import { getWeekTop10 } from "./utils/requests";
+import { fanFavorite, getWeekTop10 } from "./utils/requests";
 
 export default async function HomePage() {
   const movies = await getWeekTop10();
+  const fanfavorite = await fanFavorite();
 
   return (
     <div className="container my-5">
@@ -28,6 +29,24 @@ export default async function HomePage() {
       </div>
       <div className="container my-5">
         <h1 className="mb-4 text-center">Fan Favorite</h1>
+      </div>
+      <div className="row g-4">
+        {fanfavorite.map((movie) => {
+          const imgUrl = movie.primaryImage.imageUrl;
+          const id = movie.id;
+          const title = movie.originalTitleText.text;
+          const description = movie.plot.plotText.plainText;
+          return (
+            <div className="col-md-4 col-lg-4" key={id}>
+              <Card
+                imageUrl={imgUrl}
+                id={id}
+                title={title}
+                description={description}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
